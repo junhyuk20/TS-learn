d122178e31c3e7da38125271a23767644b9bcf35
junhyuk20 <kwk8002@naver.com>
2022-06-02 10:58:49 +0900

    수정

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -15,6 +15,6 @@ class Person11 {
 let developer11: Developer11;
 let person11: Person11;
 
-developer11 = person11; // 대입을 받는 쪽의 필요속성 갯수를 맞춰 주어야 되는데, person11은 하나밖에 없으므로 호환이 안된다.
+developer11 = new Person11; // 대입을 받는 쪽의 필요속성 갯수를 맞춰 주어야 되는데, person11은 하나밖에 없으므로 호환이 안된다.
 
 person11 = developer11; // 반대로 대입하는 쪽의 갯수가 많고 받는 쪽이 적으면 타입 호환가능하다. 왜? developer11가 person11에 필요한 모든 걸 가지고 있으니깐
